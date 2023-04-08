71645136c11b1d1d50d8a3c3832da2b80fa0630e
Ogu1208 <kdasunb@naver.com>
2023-04-08 16:26:10 +0900

    Update: Diary Service
    1. Get data from open weather map
    2.  Parsing weather data received
    3. sabe to DB

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,55 +1,106 @@
 package zerobase.weather.service;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import zerobase.weather.domain.Diary;
+import zerobase.weather.repository.DiaryRepository;
+
+import java.net.HttpURLConnection;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class DiaryService {
+
     @Value("${openweathermap.key}")
     private String apiKey;
-    public void createDiary(LocalDate date, String text) {
-        // open weathermap에서 날씨 데이터 가져오기
-        String weatherData = getWeatherString();
+    private final DiaryRepository diaryRepository;
 
-        // 받아온 날씨 json 파싱하기
-
-        // 파싱된 데이터 + 일기 값 우리 db에 넣기
+    public DiaryService(DiaryRepository diaryRepository) {
+        this.diaryRepository = diaryRepository;
     }
 
-    // open weathermap에서 날씨 데이터 가져오기
-    private String getWeatherString(){
-        // open weather map에서 데이터 받아오기
-        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
+    public void createDiary(LocalDate date, String text) {
+        // open weather map 에서 데이터 받아오기
+        String weatherData = getWeatherString();
 
+        // 받아온 날씨 데이터 파싱하기
+        Map<String, Object> parseWeather = parseWeather(weatherData);
+
+        // 우리 db에 저장하기
+        Diary nowDiary = new Diary();
+        nowDiary.setWeather(parseWeather.get("main").toString());
+        nowDiary.setIcon(parseWeather.get("icon").toString());
+        nowDiary.setTemperature((Double) parseWeather.get("temp"));
+        nowDiary.setText(text);
+        nowDiary.setDate(date);
+        diaryRepository.save(nowDiary);
+
+    }
+
+    private String getWeatherString() {
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
         try {
             URL url = new URL(apiUrl);
-            // api url을 Http형식으로 연결시킴
+            // api url을 Http 형식으로 연결시킴
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             int responseCode = connection.getResponseCode();
+
+//          BufferedReader에 오류 메시지나 응답 객체를 받아옴
             BufferedReader br;
-            if(responseCode == 200) {
+            if (responseCode == 200) {
                 br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             } else {
                 br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
             }
+
+//          BufferedReader를 한줄씩 읽으면서 StringBuilder response에 붙임
             String inputLine;
             StringBuilder response = new StringBuilder();
-            while((inputLine = br.readLine()) != null) {
+            while ((inputLine = br.readLine()) != null) {
                 response.append(inputLine);
             }
             br.close();
             return response.toString();
+
         } catch (Exception e) {
-            return "failed to get response";
+            e.printStackTrace();    //예외정보 출력
+            return e.getMessage();
+//            return "failed to get response";
         }
 
     }
+
+    private Map<String, Object> parseWeather(String jsonString) {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        Map<String, Object> resultMap = new HashMap<>();
+
+        JSONObject mainData = (JSONObject) jsonObject.get("main");
+        resultMap.put("temp",   mainData.get("temp"));
+        JSONArray weatherArray = (JSONArray) jsonObject.get("weather");
+        JSONObject weatherData = (JSONObject) weatherArray.get(0);
+        resultMap.put("main", weatherData.get("main"));
+        resultMap.put("icon", weatherData.get("icon"));
+
+        return resultMap;
+    }
+
 }
